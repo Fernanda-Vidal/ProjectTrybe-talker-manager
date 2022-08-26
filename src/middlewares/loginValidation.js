924f3161fcbbda4550f67879af91ef5793939d22
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-26 19:30:03 -0300

    Cria validação do login. Req 4.

--- src/middlewares/loginValidation.js
+++ src/middlewares/loginValidation.js
@@ -1,15 +1,23 @@
 const loginValidation = (req, res, next) => {
     const { email, password } = req.body;
-
+    const isValid = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
     if (!email) {
-        return res.status(400).json({ message: 'Please provide a valid e-mail' });
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
     }
-
+    if (!isValid.test(email)) {
+        return res.status(400)
+        .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
     if (!password) {
-        return res.status(400).json({ message: 'Invalid password' });
+        return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    }
+    if (password.length < 6) {
+        return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
 
     next();
 };
 
 module.exports = loginValidation;
+
+// crédito: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
