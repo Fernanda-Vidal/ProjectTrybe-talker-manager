6cef7b67b8decf384fed048b3208cecc30966b24
Fernanda Vidal <fevidal.dev@gmail.com>
2022-08-26 18:14:31 -0300

    Cria endpoint GET talker/:id.

--- src/routes/routesTalker.js
+++ src/routes/routesTalker.js
@@ -8,4 +8,14 @@ routesTalker.get('/talker', async (req, res) => {
     return res.status(200).json(talkers);
 });
 
+routesTalker.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkers = await readTalkersFile(); 
+    const talkerId = talkers.find((talk) => talk.id === Number(id));
+    if (!talkerId) {
+        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    } 
+    return res.status(200).json(talkerId);
+});
+
 module.exports = routesTalker;
